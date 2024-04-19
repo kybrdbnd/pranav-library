0928412af3c58eb73e9881c559636a55655948cc
Pranav Puri <pranavpuri.p@hotmail.com>
2024-04-19 20:19:41 +0530

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Pydelhi module',
+    name='pydelhi',
     version='1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     install_requires=[
         'python-dateutil'
     ]
